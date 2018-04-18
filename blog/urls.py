2e51e451ee326e24d641a64732c2b39a94d63cb4
ells ye <ells@dasudian.com>
2018-04-18 10:48:17 +0800

    new project

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('post/<pk>/', views.detail, name='detail'),
+    path('archives/<year>/<month>/', views.archives, name='archives'),
+    path('category/<pk>/', views.category, name='category'),
+]
